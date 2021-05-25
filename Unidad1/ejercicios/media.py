d794a8d0baf6bba65a62825415692bc13a2837a0
sergio perez <sergio-luis98@hotmail.com>
2021-05-24 20:55:08 -0500

    media aritmetica

---
+++ Unidad1/ejercicios/media.py
@@ -0,0 +1,11 @@
+def run():
+  valor_1 = int(input('Ingrese el primer numero: '))
+  valor_2 = int(input('Ingrese el segundo numero: '))
+  valor_3 = int(input('Ingrese el tercer numero: '))
+  media = (valor_1 + valor_2 + valor_3)/3
+  
+  print(f'la media aritmetica de los numeros {valor_1}, {valor_2}, {valor_3} es = {media}')
+  
+
+if __name__ == "__main__":
+    run()
